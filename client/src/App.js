aa8d2c6135d9294e5c0ca24ca59902fb84afd1fc
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-10-27 22:08:19 +0900

    git 실수 수정..

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,34 @@
 // 2. 라이브러리를 사용하는 방법 - axios
 
 import { useEffect, useState } from "react";
-import axios from "axios";
 
-const SERVER_URL = "http://localhost:4000/api/todo";
 function App() {
   const [todoList, setTodoList] = useState(null);
 
-  const fetchData = async () => {
-    const response = await axios.get(SERVER_URL);
-    setTodoList(response.data);
+  const fetchData = () => {
+    fetch("http://localhost:4000/api/todo")
+      .then((response) => response.json())
+      .then((data) => setTodoList(data));
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const onSubmitHandler = async (event) => {
+  const onSubmitHandler = (event) => {
     event.preventDefault();
     const text = event.target.text.value;
     const done = event.target.done.checked;
-    await axios.post(SERVER_URL, { text, done });
-    fetchData();
+    fetch("http://localhost:4000/api/todo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        done,
+      }),
+    }).then(() => fetchData());
   };
 
   return (
